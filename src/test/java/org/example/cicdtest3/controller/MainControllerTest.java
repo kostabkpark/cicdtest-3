ab314414ddca09a3013ffa1b73e70a5230927def
kostabkpark <kostabkpark@gmail.com>
2024-11-27 15:30:00 +0900

    test code add - 2

--- src/test/java/org/example/cicdtest3/controller/MainControllerTest.java
+++ src/test/java/org/example/cicdtest3/controller/MainControllerTest.java
@@ -1,5 +1,6 @@
 package org.example.cicdtest3.controller;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,5 +11,6 @@ class MainControllerTest {
 
     @Test
     void main() {
+        Assertions.assertThat(true).isTrue();
     }
 }
